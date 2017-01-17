67b8f70b79c3e9026fbd1040e21395c8473d01ce
greynix <greynix@gmail.com>
2017-01-17 20:02:37 +0300

    fix README and bars.py

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+Скрипт получает на вход json файл и выводит информацию о:
+- Баре с наименьшим колличеством посадочных мест
+- Баре с наибольшим колличеством посадочных мест
+- Ближайшем к заданым координатам баре
 
 # Как запустить
 
@@ -11,7 +14,7 @@
 ```#!bash
 
 $ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+{'Name': 'Спорт бар «Красная машина»', 'IsNetObject': 'нет', 'AdmArea': 'Южный административный округ', 'SeatsCount': 450, 'system_object_id': '00138530', 'PublicPhone': [{'global_object_id': 169375059.0, 'PublicPhone': '(905) 795-15-84', 'system_object_id': '00138530 _1', 'global_id': 33761.0}], 'geoData': {'type': 'Point', 'coordinates': [37.638228501070095, 55.70111462948684]}, 'global_id': 169375059, 'District': 'Даниловский район', 'Longitude_WGS84': '37.6382285008039050', 'ID': '00138530', 'TypeObject': 'бар', 'Latitude_WGS84': '55.7011146292467670', 'Address': 'Автозаводская улица, дом 23, строение 1', 'SocialPrivileges': 'нет'}
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,21 +1,65 @@
+#!/usr/bin/env python3
 import json
+import argparse
+from math import radians, cos, sin, asin, sqrt
 
 
-def load_data(filepath):
-    pass
+def load_data(path): 
+    """Open json file (cp1251)"""
+    with open(path, encoding='cp1251') as json_file:
+        data = json.load(json_file)
+    return data    
 
 
 def get_biggest_bar(data):
-    pass
+    """Some python magic"""
+    return max(data, key=lambda k: k['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    pass
+    """Some python magic"""
+    return min(data, key=lambda k: k['SeatsCount'])
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    """ Haversine formula """
+    lat2 = radians(latitude)
+    lon2 = radians(longitude)
+    d = None
+    for i in range(len(data)):
+        lat = radians(float(data[i]['Latitude_WGS84']))
+        lon = radians(float(data[i]['Longitude_WGS84']))
+        dlon = lon2 - lon 
+        dlat = lat2 - lat 
+        a = sin(dlat/2)**2 + cos(lat) * cos(lat2) * sin(dlon/2)**2
+        c = 2 * asin(sqrt(a)) 
+        r = 6371 
+        if d is None or c * r < d:
+            d = c * r 
+            bar = i
+        else:
+            continue
+    return data[bar]   
 
 
 if __name__ == '__main__':
-    pass
+    parser=argparse.ArgumentParser(description='''Bars info.''')
+    subparsers = parser.add_subparsers (dest='command')
+    biggest_parser = subparsers.add_parser('biggest', help='find biggest bar')
+    smallest_parser = subparsers.add_parser('smallest', help='find smallest bar')
+    closest_parser = subparsers.add_parser('closest', help='find closest bar')
+    closest_group = closest_parser.add_argument_group (title='find closest bar')
+    closest_group.add_argument('latitude', type=float)
+    closest_group.add_argument('longitude', type=float)
+    parser.add_argument('-json_file', type=str, default='data.json', help='path to file')
+    
+    N = parser.parse_args()
+    
+    if N.command == 'biggest':
+        print(get_biggest_bar(load_data(N.json_file)))
+    elif N.command == 'smallest':
+        print(get_smallest_bar(load_data(N.json_file)))
+    elif N.command == 'closest':
+        print(get_closest_bar(load_data(N.json_file),N.longitude,N.latitude))
+    else:
+        print(get_biggest_bar(load_data(N.json_file)))
